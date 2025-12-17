34a0cacc3fd89ad612c09825ef066e50a69190a1
PatcharaP <itt.patchara@gmail.com>
2025-12-17 10:16:04 +0700

    init commit with tailwind,axios,shadcn,routerdom

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline" className="bg-amber-100">Button</Button>
+    </>
+  );
+}
+
+export default App;
